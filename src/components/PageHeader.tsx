29dcf9ba71c35c0482d09ec8fc02fa741944e420
Bukola Omojowo <bomojowo@gmail.com>
2021-06-16 14:32:11 +0100

    Formateed used prettier git status clear

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -2,7 +2,9 @@ function PageHeader(): JSX.Element {
   return (
     <header>
       <h1>The Web Page</h1>
-      <p>Welcome to Bukky'<s></s> Web Page!</p>
+      <p>
+        Welcome to Bukky'<s></s> Web Page!
+      </p>
       <p>
         This shows a random scatter of my thoughts, last updated on June 16,
         2003.
